676cee304dd5a196e6fc46f60dad334d4a86a845
Enderward <enderward@enderward-Presario-CQ43-Notebook-PC.(none)>
2013-07-09 23:32:32 -0500

    checkpoint inicial

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,5 @@
+class StaticPagesController < ApplicationController
+  def index
+    @variable = ["Prueba", "Prueba2","Prueba3", "Prueba4"]
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NuevaApp::Application.config.secret_key_base = 'ce4e383d67c0297651aa09c1fc1177bc08a274bf644de2a547f1b2cfca45346dbf517769729c9c2e89c4b388c661d6b2483d3019ff57cd564986fb6940fd9002'
